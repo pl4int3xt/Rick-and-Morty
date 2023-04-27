6a13c3cccdcb29eab88847c0cfd449b0810373a7
benjahjp <benjahjp@gmail.com>
2023-04-27 21:46:11 +0300

    feat: project setup

---
+++ app/src/main/java/com/example/rickandmorty/data/dto/Location.kt
@@ -0,0 +1,6 @@
+package com.example.rickandmorty.data.dto
+
+data class Location(
+    val name: String,
+    val url: String
+)
---
+++ app/src/main/java/com/example/rickandmorty/data/dto/Result.kt
@@ -0,0 +1,6 @@
+package com.example.rickandmorty.data.dto
+
+data class Result(
+    val info: Info,
+    val results: List<CharacterDto>
+)
