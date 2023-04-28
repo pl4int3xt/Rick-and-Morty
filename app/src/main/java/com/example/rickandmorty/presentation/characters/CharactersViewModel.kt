3f7f29c457bd867c4be3190a267b2c0dca5a3356
benjahjp <benjahjp@gmail.com>
2023-04-28 12:04:06 +0300

    feat: characters viewModel

--- app/src/main/java/com/example/rickandmorty/presentation/characters/CharactersViewModel.kt
+++ app/src/main/java/com/example/rickandmorty/presentation/characters/CharactersViewModel.kt
@@ -1,4 +1,11 @@
 package com.example.rickandmorty.presentation.characters
 
-class CharactersViewModel {
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class CharactersViewModel @Inject constructor(
+    private val
+): ViewModel() {
 }
