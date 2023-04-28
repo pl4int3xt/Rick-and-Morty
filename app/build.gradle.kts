57bf34a55ac1e33b351517c3881513bf8b7006a7
benjahjp <benjahjp@gmail.com>
2023-04-28 10:57:21 +0300

    feat: dependencies kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,40 +71,39 @@ dependencies {
 
 
     // Compose dependencies
-    implementation ("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.0-alpha04")
-    implementation ("androidx.navigation:navigation-compose:2.6.0-alpha04")
-    implementation ("androidx.compose.material:material-icons-extended:1.3.1")
-    implementation ("androidx.hilt:hilt-navigation-compose:1.0.0")
+    implementation (libs.androidx.lifecycle.viewmodel.compose)
+    implementation (libs.androidx.navigation.compose)
+    implementation (libs.androidx.material.icons.extended)
+    implementation (libs.androidx.hilt.navigation.compose)
 
     // Coroutines
-    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
-    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
+    implementation (libs.kotlinx.coroutines.core)
+    implementation (libs.kotlinx.coroutines.android)
 
     //Dagger - Hilt
-    implementation ("com.google.dagger:hilt-android:2.44")
-    kapt("com.google.dagger:hilt-compiler:2.44")
-    kapt("androidx.hilt:hilt-compiler:1.0.0")
+    implementation (libs.dagger.hilt.android)
+    kapt(libs.hilt.compiler)
+    kapt(libs.androidx.hilt.compiler)
 
     //navigation animation
-    implementation ("com.google.accompanist:accompanist-navigation-animation:0.28.0")
+    implementation (libs.accompanist.navigation.animation)
 
     //retrofit
-    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation ("com.squareup.okhttp3:okhttp:5.0.0-alpha.2")
-    testImplementation ("com.squareup.okhttp3:mockwebserver:4.9.1")
+    implementation (libs.retrofit)
+    implementation (libs.converter.gson)
+    implementation (libs.okhttp)
+    testImplementation (libs.mockwebserver)
 
     //coil
-    implementation ("io.coil-kt:coil-compose:2.2.2")
+    implementation (libs.coil.compose)
 
     //lottie
-    implementation ("com.airbnb.android:lottie-compose:5.2.0")
+    implementation (libs.lottie.compose)
 
     //splash api
-    implementation ("androidx.core:core-splashscreen:1.0.0")
+    implementation (libs.androidx.core.splashscreen)
 
     //pagination
-    //paging
-    implementation ("androidx.paging:paging-compose:1.0.0-alpha18")
-    implementation ("androidx.paging:paging-runtime-ktx:3.1.1")
+    implementation (libs.androidx.paging.compose)
+    implementation (libs.androidx.paging.runtime.ktx)
 }
