5fe21fe40cb5c25081c684a1805df0c6858682db
benjahjp <benjahjp@gmail.com>
2023-04-29 17:00:41 +0300

    feat: character details

--- app/src/main/java/com/example/rickandmorty/presentation/character_details/components/Image.kt
+++ app/src/main/java/com/example/rickandmorty/presentation/character_details/components/Image.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -28,7 +27,7 @@ fun Image(
     ) {
         AsyncImage(
             modifier = Modifier.fillMaxWidth()
-                .fillMaxHeight(0.9f)
+                .fillMaxHeight(1f)
                 .clip(RoundedCornerShape(topEnd = 30.dp, topStart = 30.dp))
             ,
             model = imageUrl,
