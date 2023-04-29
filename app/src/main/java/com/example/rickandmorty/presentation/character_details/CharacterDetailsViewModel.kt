65b72d122aacb1a498864d688cdeb9a6a60838df
benjahjp <benjahjp@gmail.com>
2023-04-29 15:10:49 +0300

    feat: character details

--- app/src/main/java/com/example/rickandmorty/presentation/character_details/CharacterDetailsViewModel.kt
+++ app/src/main/java/com/example/rickandmorty/presentation/character_details/CharacterDetailsViewModel.kt
@@ -1,4 +1,22 @@
 package com.example.rickandmorty.presentation.character_details
 
-class CharacterDetailsViewModel {
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import com.example.rickandmorty.domain.use_case.GetCharacterDetailsUseCase
+import kotlinx.coroutines.flow.onEach
+import javax.inject.Inject
+
+class CharacterDetailsViewModel @Inject constructor(
+    private val getCharacterDetailsUseCase: GetCharacterDetailsUseCase,
+    savedStateHandle: SavedStateHandle
+): ViewModel() {
+    init {
+        savedStateHandle
+    }
+
+    fun getCharacterDetails(id: Int){
+        getCharacterDetailsUseCase(id).onEach {
+
+        }
+    }
 }
