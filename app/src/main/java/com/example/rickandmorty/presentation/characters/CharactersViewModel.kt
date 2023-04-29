4d53e58da391263d146b0f65824ba24d25569a67
benjahjp <benjahjp@gmail.com>
2023-04-29 10:48:44 +0300

    feat: get all characters use case

--- app/src/main/java/com/example/rickandmorty/presentation/characters/CharactersViewModel.kt
+++ app/src/main/java/com/example/rickandmorty/presentation/characters/CharactersViewModel.kt
@@ -3,23 +3,22 @@ package com.example.rickandmorty.presentation.characters
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.paging.Pager
+import androidx.paging.PagingData
 import androidx.paging.cachedIn
 import androidx.paging.map
 import com.example.rickandmorty.data.mappers.toCharacterModel
 import com.example.rickandmorty.data.remote.dto.CharacterDto
+import com.example.rickandmorty.domain.model.CharacterModel
+import com.example.rickandmorty.domain.use_case.GetAllCharactersUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 @HiltViewModel
 class CharactersViewModel @Inject constructor(
-    private val pager: Pager<Int, CharacterDto>
+    private val getAllCharactersUseCase: GetAllCharactersUseCase
 ): ViewModel() {
 
-    val characters = pager
-        .flow
-        .map { pagingData ->
-            pagingData.map { it.toCharacterModel() }
-        }
-        .cachedIn(viewModelScope)
+    val characters = getAllCharactersUseCase().cachedIn(viewModelScope)
 }
