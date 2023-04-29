b415e62b44f6a7c212854e66807c39436ce7b20a
benjahjp <benjahjp@gmail.com>
2023-04-29 11:27:16 +0300

    feat: character screen

--- app/src/main/java/com/example/rickandmorty/presentation/uiEvents/UiEvents.kt
+++ app/src/main/java/com/example/rickandmorty/presentation/uiEvents/UiEvents.kt
@@ -1,3 +1,7 @@
 package com.example.rickandmorty.presentation.uiEvents
 
-sealed class UiEvents
+sealed class UiEvents{
+    object OnPopBackStack: UiEvents()
+    data class OnNavigate(val route: String): UiEvents()
+    data class OnShowToast(val message: String): UiEvents()
+}
