39d00e7c49c02c2998908ae0870689f1719bbca5
benjahjp <benjahjp@gmail.com>
2023-04-29 10:40:20 +0300

    feat: characters screen

--- app/src/main/java/com/example/rickandmorty/presentation/characters/components/SingleItem.kt
+++ app/src/main/java/com/example/rickandmorty/presentation/characters/components/SingleItem.kt
@@ -3,9 +3,12 @@ package com.example.rickandmorty.presentation.characters.components
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 
 @Composable
@@ -16,10 +19,13 @@ fun SingleItem(
 ) {
     Box(
         modifier = Modifier.height(item.height)
+            .clip(RoundedCornerShape(16.dp))
             .background(color = item.color)
     ){
         AsyncImage(
-            modifier = Modifier.height(item.height),
+            modifier = Modifier.height(item.height)
+                .clip(RoundedCornerShape(16.dp))
+            ,
             model = imageUrl,
             contentScale = ContentScale.Crop,
             contentDescription = name,
