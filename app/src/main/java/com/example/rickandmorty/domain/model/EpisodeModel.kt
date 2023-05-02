98b4cd9595684ba41eff71eb069f67a7eb3acb02
benjahjp <benjahjp@gmail.com>
2023-05-02 14:45:36 +0300

    feat: models

--- app/src/main/java/com/example/rickandmorty/domain/model/EpisodeModel.kt
+++ app/src/main/java/com/example/rickandmorty/domain/model/EpisodeModel.kt
@@ -1,3 +1,11 @@
 package com.example.rickandmorty.domain.model
 
-data class EpisodeModel()
+data class EpisodeModel(
+    val airDate: String,
+    val characters: List<String>,
+    val created: String,
+    val episode: String,
+    val id: Int,
+    val name: String,
+    val url: String
+)
--- app/src/main/java/com/example/rickandmorty/domain/model/LocationModel.kt
+++ app/src/main/java/com/example/rickandmorty/domain/model/LocationModel.kt
@@ -1,3 +1,11 @@
 package com.example.rickandmorty.domain.model
 
-data class LocationModel()
+data class LocationModel(
+    val created: String,
+    val dimension: String,
+    val id: Int,
+    val name: String,
+    val residents: List<String>,
+    val type: String,
+    val url: String
+)
