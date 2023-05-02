11d196cc6827c69fd0288d06a3a8ce9314383c24
benjahjp <benjahjp@gmail.com>
2023-05-02 15:23:18 +0300

    fix: repository

--- app/src/main/java/com/example/rickandmorty/data/remote/repository/RepositoryImpl.kt
+++ app/src/main/java/com/example/rickandmorty/data/remote/repository/RepositoryImpl.kt
@@ -2,6 +2,10 @@ package com.example.rickandmorty.data.remote.repository
 
 import com.example.rickandmorty.data.remote.api.RickAndMortyApi
 import com.example.rickandmorty.data.remote.dto.CharacterDto
+import com.example.rickandmorty.data.remote.dto.EpisodeDto
+import com.example.rickandmorty.data.remote.dto.EpisodeResult
+import com.example.rickandmorty.data.remote.dto.LocationDto
+import com.example.rickandmorty.data.remote.dto.LocationResult
 import com.example.rickandmorty.data.remote.dto.Result
 import com.example.rickandmorty.domain.repository.Repository
 import javax.inject.Inject
@@ -15,5 +19,21 @@ class RepositoryImpl @Inject constructor(
 
     override suspend fun getCharacterDetails(id: Int): CharacterDto {
         return rickAndMortyApi.getCharacterDetails(id)
+    }
+
+    override suspend fun getAllEpisodes(page: Int): EpisodeResult {
+        return rickAndMortyApi.getAllEpisodes(page)
+    }
+
+    override suspend fun getEpisodeDetails(id: Int): EpisodeDto {
+        return rickAndMortyApi.getEpisodeDetails(id)
+    }
+
+    override suspend fun getAllLocations(page: Int): LocationResult {
+        return rickAndMortyApi.getAllLocations(page)
+    }
+
+    override suspend fun getLocationDetails(id: Int): LocationDto {
+        return rickAndMortyApi.getLocationDetails(id)
     }
 }
--- app/src/main/java/com/example/rickandmorty/domain/repository/Repository.kt
+++ app/src/main/java/com/example/rickandmorty/domain/repository/Repository.kt
@@ -1,10 +1,18 @@
 package com.example.rickandmorty.domain.repository
 
 import com.example.rickandmorty.data.remote.dto.CharacterDto
+import com.example.rickandmorty.data.remote.dto.EpisodeDto
+import com.example.rickandmorty.data.remote.dto.EpisodeResult
+import com.example.rickandmorty.data.remote.dto.LocationDto
+import com.example.rickandmorty.data.remote.dto.LocationResult
 import com.example.rickandmorty.data.remote.dto.Result
 
 interface Repository {
 
     suspend fun getAllCharacters(page: Int): Result
     suspend fun getCharacterDetails(id: Int): CharacterDto
+    suspend fun getAllEpisodes(page: Int): EpisodeResult
+    suspend fun getEpisodeDetails(id: Int): EpisodeDto
+    suspend fun getAllLocations(page: Int): LocationResult
+    suspend fun getLocationDetails(id: Int): LocationDto
 }
