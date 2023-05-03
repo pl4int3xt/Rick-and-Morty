9d78d7470cec3053646bd8ab7ed87794c3f46057
benjahjp <benjahjp@gmail.com>
2023-05-03 20:59:38 +0300

    feat: episode view model

--- app/src/main/java/com/example/rickandmorty/presentation/episodes/EpisodesScreenViewModel.kt
+++ app/src/main/java/com/example/rickandmorty/presentation/episodes/EpisodesScreenViewModel.kt
@@ -1,4 +1,22 @@
 package com.example.rickandmorty.presentation.episodes
 
-class EpisodesScreenViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.Pager
+import androidx.paging.cachedIn
+import androidx.paging.map
+import com.example.rickandmorty.data.mappers.toEpisodeModel
+import com.example.rickandmorty.data.remote.dto.EpisodeDto
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+
+class EpisodesScreenViewModel @Inject constructor(
+    private val pager: Pager<Int, EpisodeDto>
+): ViewModel() {
+    val episodes = pager
+        .flow
+        .map { pagingData ->
+            pagingData.map { it.toEpisodeModel() }
+        }
+        .cachedIn(viewModelScope)
 }
