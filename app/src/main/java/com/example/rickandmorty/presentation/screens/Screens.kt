9c78dfafdb3086e7800016973866029578c3f3cc
benjahjp <benjahjp@gmail.com>
2023-05-03 21:06:41 +0300

    feat: screens

--- app/src/main/java/com/example/rickandmorty/presentation/screens/Screens.kt
+++ app/src/main/java/com/example/rickandmorty/presentation/screens/Screens.kt
@@ -3,6 +3,8 @@ package com.example.rickandmorty.presentation.screens
 sealed class Screens(val route: String) {
     object CharactersScreen: Screens("characters_screen")
     object CharacterDetails: Screens("character_details_screen")
-    object LocationScreen: Screens("characters_screen")
+    object LocationScreen: Screens("location_screen")
+    object LocationDetailsScreen: Screens("location_details_screen")
     object EpisodeScreen: Screens("episode_screen")
+    object EpisodeDetailsScreen: Screens("episode_details_screen")
 }
