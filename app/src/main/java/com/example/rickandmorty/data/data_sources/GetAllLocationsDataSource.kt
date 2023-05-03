d20e023c6a8e3a443a99b2138da91a5b1227f568
benjahjp <benjahjp@gmail.com>
2023-05-03 10:09:15 +0300

    feat: get all locations data source

---
+++ app/src/main/java/com/example/rickandmorty/data/data_sources/GetAllLocationsDataSource.kt
@@ -0,0 +1,29 @@
+package com.example.rickandmorty.data.data_sources
+
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import com.example.rickandmorty.data.remote.dto.LocationDto
+import com.example.rickandmorty.domain.repository.Repository
+import javax.inject.Inject
+
+class GetAllLocationsDataSource @Inject constructor(
+    private val repository: Repository
+): PagingSource<Int, LocationDto>() {
+    override fun getRefreshKey(state: PagingState<Int, LocationDto>): Int? {
+        return null
+    }
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, LocationDto> {
+        return try {
+            val currentPage = params.key?:1
+            val response = repository.getAllLocations(currentPage)
+            LoadResult.Page(
+                data = response.results,
+                prevKey = if (currentPage == 1) null else -1,
+                nextKey = currentPage.plus(1)
+            )
+        } catch (e: Exception){
+            LoadResult.Error(e)
+        }
+    }
+}
