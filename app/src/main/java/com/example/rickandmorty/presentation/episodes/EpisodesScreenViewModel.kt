0c1391bd7027afd5657f57bb5c9034edf02c04c3
benjahjp <benjahjp@gmail.com>
2023-05-03 21:08:01 +0300

    feat: episodes view model

--- app/src/main/java/com/example/rickandmorty/presentation/episodes/EpisodesScreenViewModel.kt
+++ app/src/main/java/com/example/rickandmorty/presentation/episodes/EpisodesScreenViewModel.kt
@@ -7,7 +7,12 @@ import androidx.paging.cachedIn
 import androidx.paging.map
 import com.example.rickandmorty.data.mappers.toEpisodeModel
 import com.example.rickandmorty.data.remote.dto.EpisodeDto
+import com.example.rickandmorty.presentation.screens.Screens
+import com.example.rickandmorty.presentation.uiEvents.UiEvents
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class EpisodesScreenViewModel @Inject constructor(
@@ -19,4 +24,22 @@ class EpisodesScreenViewModel @Inject constructor(
             pagingData.map { it.toEpisodeModel() }
         }
         .cachedIn(viewModelScope)
+
+    private val _uiEvents = MutableSharedFlow<UiEvents>()
+    val uiEvents = _uiEvents.asSharedFlow()
+
+    fun onEvent(episodesScreenEvents: EpisodesScreenEvents){
+        when(episodesScreenEvents){
+            is EpisodesScreenEvents.OnEpisodeClicked -> {
+                viewModelScope.launch {
+                    _uiEvents.emit(UiEvents.OnNavigate(Screens.EpisodeDetailsScreen.route +"/${episodesScreenEvents.id}"))
+                }
+            }
+            EpisodesScreenEvents.OnPopBackStack -> {
+                viewModelScope.launch {
+                    _uiEvents.emit(UiEvents.OnPopBackStack)
+                }
+            }
+        }
+    }
 }
