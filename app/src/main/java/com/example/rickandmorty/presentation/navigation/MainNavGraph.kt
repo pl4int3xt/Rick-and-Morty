06447aca797d558f3b0c9b9f709cc6d767a8e68d
benjahjp <benjahjp@gmail.com>
2023-05-04 18:27:08 +0300

    feat: episode screen

--- app/src/main/java/com/example/rickandmorty/presentation/navigation/MainNavGraph.kt
+++ app/src/main/java/com/example/rickandmorty/presentation/navigation/MainNavGraph.kt
@@ -11,6 +11,8 @@ import com.example.rickandmorty.presentation.character_details.CharacterDetailsV
 import com.example.rickandmorty.presentation.character_details.components.CharacterDetailsScreen
 import com.example.rickandmorty.presentation.characters.CharactersViewModel
 import com.example.rickandmorty.presentation.characters.components.CharactersScreen
+import com.example.rickandmorty.presentation.episodes.EpisodesScreenViewModel
+import com.example.rickandmorty.presentation.episodes.components.EpisodeScreen
 import com.example.rickandmorty.presentation.screens.Screens
 
 @Composable
@@ -41,7 +43,15 @@ fun MainNavGraph(
                 state = state)
         }
         composable(Screens.EpisodeScreen.route){
-
+            val viewModel: EpisodesScreenViewModel = hiltViewModel()
+            val uiEvents = viewModel.uiEvents
+            val episodes = viewModel.episodes.collectAsLazyPagingItems()
+            EpisodeScreen(
+                onNavigate = { navHostController.navigate(it.route)},
+                uiEvents = uiEvents,
+                onEvent = viewModel::onEvent,
+                episodes = episodes
+            )
         }
         composable(Screens.LocationScreen.route){
 
