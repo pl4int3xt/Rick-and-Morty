c1d100505b4675175d678183da2d5d8b6da1cb21
benjahjp <benjahjp@gmail.com>
2023-05-04 18:42:30 +0300

    feat: Location navigation graph

--- app/src/main/java/com/example/rickandmorty/MainActivity.kt
+++ app/src/main/java/com/example/rickandmorty/MainActivity.kt
@@ -5,7 +5,6 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.navigation.compose.rememberNavController
 import com.example.rickandmorty.presentation.main.components.MainScreen
-import com.example.rickandmorty.presentation.navigation.MainNavGraph
 import com.example.rickandmorty.presentation.theme.RickAndMortyTheme
 import dagger.hilt.android.AndroidEntryPoint
 
