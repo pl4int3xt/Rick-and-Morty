a5de4a946c7a8a7ffc03b4f2bbad9fdf0ae4fb1f
benjahjp <benjahjp@gmail.com>
2023-05-04 18:31:05 +0300

    feat: location screen

--- app/src/main/java/com/example/rickandmorty/presentation/navigation/MainNavGraph.kt
+++ app/src/main/java/com/example/rickandmorty/presentation/navigation/MainNavGraph.kt
@@ -2,7 +2,6 @@ package com.example.rickandmorty.presentation.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
