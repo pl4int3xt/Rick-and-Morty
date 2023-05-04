18186ee41db27d0841056a0e9ccb0573aa31d763
benjahjp <benjahjp@gmail.com>
2023-05-04 17:13:45 +0300

    feat: singleitem

--- app/src/main/java/com/example/rickandmorty/presentation/characters/components/SingleItem.kt
+++ app/src/main/java/com/example/rickandmorty/presentation/characters/components/SingleItem.kt
@@ -3,13 +3,22 @@ package com.example.rickandmorty.presentation.characters.components
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 
 @Composable
@@ -20,18 +29,25 @@ fun SingleItem(
     onClick: () -> Unit
 ) {
     Box(
-        modifier = Modifier.height(item.height)
+        modifier = Modifier
+            .height(500.dp)
             .clip(RoundedCornerShape(16.dp))
             .background(color = item.color)
-            .clickable { onClick() }
+            .clickable { onClick() },
+        contentAlignment = Alignment.BottomCenter
     ){
         AsyncImage(
-            modifier = Modifier.height(item.height)
-                .clip(RoundedCornerShape(16.dp))
-            ,
+            modifier = Modifier.fillMaxSize(),
             model = imageUrl,
             contentScale = ContentScale.Crop,
             contentDescription = name,
+        )
+        Text(
+            modifier = Modifier.fillMaxWidth()
+                .padding(16.dp)
+                .background(color = Color.White),
+            text = name,
+            fontSize = 30.sp,
         )
     }
 }
